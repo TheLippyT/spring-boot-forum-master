114985a8c86e78a0039267ebbd65f1d7ab200e5b
Dean Martodikromo <dmartodikromo@qualogy.com>
2021-07-31 15:58:08 -0300

    ui changes. more required

--- src/main/resources/_data.sql
+++ src/main/resources/_data.sql
@@ -1,13 +1,8 @@
 -- init data
 
 -- User Data
-INSERT INTO `T_USER` (username, password, email, activated, date_created, bio, roles) VALUES ('admin', '$2a$10$HiaJpKERbSfcZcuG5vcvlOZDG6Y/5SkDKi9RvKsSK3t3Spl9.7u6u', 'admin@admin.com', 1, '2017-11-02', 'admin bio', 'ADMIN,USER');
-INSERT INTO `T_USER` (username, password, email, activated, date_created, bio, roles) VALUES ('user', '$2a$10$Dr2lvPjmjiMuWf3mcMlyQeSbxFHI5LDLudHIB/fK/uygI1pnKk/3m', 'user@user.com', 1, '2017-11-02', 'user bio', 'USER');
 
 -- Category Data
-INSERT INTO `T_CATEGORY` (name, weight, display_name, username, date_created) VALUES ('java', 1, 'Java', 'admin', '2017-11-02');
-INSERT INTO `T_CATEGORY` (name, weight, display_name, username, date_created) VALUES ('python', 2, 'Python', 'user', '2017-11-02');
-INSERT INTO `T_CATEGORY` (name, weight, display_name, username, date_created) VALUES ('job-hunting', 3, 'Job hunting', 'user', '2017-11-02');
 
 -- Post Data
 INSERT INTO `T_POST` (title, body, date_created, user_id, category_id) VALUES (
